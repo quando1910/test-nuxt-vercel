602ea568e5e768ba3282a1c3014997a38e5bb9c4
Quan Do <q.do@shopware.com>
2024-08-06 20:13:20 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <ul>
+      <li><NuxtLink :to="'/'">Home</NuxtLink></li>
+      <li><NuxtLink :to="'/about'">About</NuxtLink></li>
+      <li><NuxtLink :to="'/contact'">Contact</NuxtLink></li>
+      <li><NuxtLink :to="'/aaaa'">AAAA</NuxtLink></li>
+    </ul>
+    <NuxtPage />
+  </div>
+</template>
